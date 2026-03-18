470737d5d751a1b76153901eabcc481dd1afdd40
Balša Bazović <balsa.bazovic@gmail.com>
2026-03-18 17:31:46 -0400

    Updated Home view.

--- pinnacle-streams/source/views/Home/Home.tsx
+++ pinnacle-streams/source/views/Home/Home.tsx
@@ -2,17 +2,21 @@
 import { useEffect } from "react";
 // IMPORTED CUSTOM COMPONENTS
 import NavPartial from "../../assets/partials/Nav/Nav";
+import HeroPartial from "../../assets/partials/views/Home/Hero/Hero";
 
 const Home = function () {
-	useEffect(() => {
-		document.title = "Pinnacle Streams | Home";
-	}, []);
+    useEffect(() => {
+        document.title = "Pinnacle Streams | Home";
+    }, []);
 
-	return (
-		<>
-			<NavPartial />
-		</>
-	);
+    return (
+        <>
+            <NavPartial />
+            <div className="div-main-edge-container">
+                <HeroPartial />
+            </div>
+        </>
+    );
 };
 
 export default Home;
