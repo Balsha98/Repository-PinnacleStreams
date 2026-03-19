99ecf9fa3133e22e8ac65de5f5d7210923d7eb90
Balša Bazović <balsa.bazovic@gmail.com>
2026-03-19 11:57:49 -0400

    Added Logo component.

---
+++ pinnacle-streams/source/assets/partials/global/Logo/Logo.tsx
@@ -0,0 +1,15 @@
+// IMPORTED CORE COMPONENTS
+import { Mountain } from "lucide-react";
+// IMPORTED STYLESHEETS
+import "./css/Logo.css";
+
+const Logo = function () {
+    return (
+        <div className="div-main-logo-container">
+            <Mountain size={24} />
+            <h2>Pinnacle</h2>
+        </div>
+    );
+};
+
+export default Logo;
