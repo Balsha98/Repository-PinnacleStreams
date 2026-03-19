42327811f10b7e47ce2826e0ae6472d75629959c
Balša Bazović <balsa.bazovic@gmail.com>
2026-03-19 11:58:00 -0400

    Updated Nav component.

--- pinnacle-streams/source/assets/partials/Nav/Nav.tsx
+++ pinnacle-streams/source/assets/partials/Nav/Nav.tsx
@@ -1,30 +1,28 @@
 // IMPORTED CORE MODULES
 import { Link } from "react-router-dom";
-import { Mountain } from "lucide-react";
 // IMPORTED STYLESHEETS
 import "./css/Nav.css";
+// IMPORTED CUSTOM MODULES
+import LogoPartial from "../global/Logo/Logo";
 
 const Nav = function () {
-	return (
-		<div className="div-main-nav-container">
-			<div className="div-main-edge-container">
-				<div className="div-main-nav-logo-container">
-					<Mountain size={24} />
-					<h2>Pinnacle</h2>
-				</div>
-				<nav className="main-nav-container">
-					<ul className="main-nav-container-links-list">
-						<li className="main-nav-container-links-list-item">
-							<Link to="/login">Sign In</Link>
-						</li>
-						<li className="main-nav-container-links-list-item">
-							<Link to="/signup">Try Free</Link>
-						</li>
-					</ul>
-				</nav>
-			</div>
-		</div>
-	);
+    return (
+        <div className="div-main-nav-container">
+            <div className="div-main-edge-container">
+                <LogoPartial />
+                <nav className="main-nav-container">
+                    <ul className="main-nav-container-links-list">
+                        <li className="main-nav-container-links-list-item">
+                            <Link to="/login">Sign In</Link>
+                        </li>
+                        <li className="main-nav-container-links-list-item">
+                            <Link to="/signup">Try Free</Link>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    );
 };
 
 export default Nav;
