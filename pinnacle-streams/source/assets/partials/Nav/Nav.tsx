940a3b93bc6c6e2115438a0a42e83b09f5f95265
Balša Bazović <balsa.bazovic@gmail.com>
2026-03-19 00:09:48 -0400

    Updated Nav component.

--- pinnacle-streams/source/assets/partials/Nav/Nav.tsx
+++ pinnacle-streams/source/assets/partials/Nav/Nav.tsx
@@ -7,20 +7,22 @@ import "./css/Nav.css";
 const Nav = function () {
 	return (
 		<div className="div-main-nav-container">
-			<div className="div-main-nav-logo-container">
-				<Mountain size={24} />
-				<h2>Pinnacle</h2>
+			<div className="div-main-edge-container">
+				<div className="div-main-nav-logo-container">
+					<Mountain size={24} />
+					<h2>Pinnacle</h2>
+				</div>
+				<nav className="main-nav-container">
+					<ul className="main-nav-container-links-list">
+						<li className="main-nav-container-links-list-item">
+							<Link to="/login">Sign In</Link>
+						</li>
+						<li className="main-nav-container-links-list-item">
+							<Link to="/signup">Try Free</Link>
+						</li>
+					</ul>
+				</nav>
 			</div>
-			<nav className="main-nav-container">
-				<ul className="main-nav-container-links-list">
-					<li className="main-nav-container-links-list-item">
-						<Link to="/login">Sign In</Link>
-					</li>
-					<li className="main-nav-container-links-list-item">
-						<Link to="/signup">Try Free</Link>
-					</li>
-				</ul>
-			</nav>
 		</div>
 	);
 };
