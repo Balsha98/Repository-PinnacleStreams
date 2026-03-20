a9312747c5adcad4e88597e9ccd77d7b0261a49a
Balša Bazović <balsa.bazovic@gmail.com>
2026-03-19 22:30:09 -0400

    Updated Footer component.

--- pinnacle-streams/source/assets/partials/global/Footer/Footer.tsx
+++ pinnacle-streams/source/assets/partials/global/Footer/Footer.tsx
@@ -102,7 +102,7 @@ const Footer = function () {
                 </div>
             </div>
             <div className="div-main-footer-copyright-container">
-                <p>@ {new Date().getFullYear()} Pinnacle Streams, Inc. All rights reserved.</p>
+                <p>&copy; {new Date().getFullYear()} Pinnacle Streams, Inc. All rights reserved.</p>
                 <ul className="main-footer-copyright-links-list">
                     <li className="main-footer-copyright-links-list-item">
                         <a href="#">Privacy Policy</a>
