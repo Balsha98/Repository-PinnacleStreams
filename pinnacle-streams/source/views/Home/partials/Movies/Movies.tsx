d8f7558540817cd291abf0ad47a4aa8591c580a0
Balša Bazović <balsa.bazovic@gmail.com>
2026-03-23 16:59:20 -0400

    Added Shows and Movies images.

--- pinnacle-streams/source/views/Home/partials/Movies/Movies.tsx
+++ pinnacle-streams/source/views/Home/partials/Movies/Movies.tsx
@@ -7,201 +7,195 @@ import type Movie from "./types/Movie";
 import "./css/Movies.css";
 
 const MOVIES: Movie[] = [
-	{
-		id: 1,
-		title: "Top Gun: Maverick",
-		details: ["2022", "PG-13", "Action", "2h 11m"],
-		badges: ["4K", "HDR"],
-		image: "http://media.themoviedb.org/t/p/w600_and_h900_face/62HCnUTziyWcpDaBO2i1DX17ljH.jpg",
-	},
-	{
-		id: 2,
-		title: "Mission Impossible — Dead Reckoning",
-		details: ["2023", "PG-13", "Action", "2h 43m"],
-		badges: ["4K", "HDR"],
-		image: "",
-	},
-	{
-		id: 3,
-		title: "Transformers: Rise of the Beasts",
-		details: ["2023", "PG-13", "Action", "2h 7m"],
-		badges: ["HD"],
-		image: "",
-	},
-	{
-		id: 4,
-		title: "A Quiet Place: Day One",
-		details: ["2024", "PG-13", "Horror", "1h 39m"],
-		badges: ["HD", "New"],
-		image: "",
-	},
-	{
-		id: 5,
-		title: "Dungeons & Dragons: Honor Among Thieves",
-		details: ["2023", "PG-13", "Fantasy", "2h 14m"],
-		badges: ["HD"],
-		image: "",
-	},
-	{
-		id: 6,
-		title: "Scream VI",
-		details: ["2023", "R", "Horror", "2h 3m"],
-		badges: ["HD"],
-		image: "",
-	},
-	{
-		id: 7,
-		title: "IF",
-		details: ["2024", "PG", "Comedy", "1h 44m"],
-		badges: ["HD", "New"],
-		image: "",
-	},
-	{
-		id: 8,
-		title: "Teenage Mutant Ninja Turtles: Mutant Mayhem",
-		details: ["2023", "PG", "Animation", "1h 39m"],
-		badges: ["HD"],
-		image: "",
-	},
-	{
-		id: 9,
-		title: "Interstellar",
-		details: ["2014", "PG-13", "Sci-Fi", "2h 49m"],
-		badges: ["4K", "HDR"],
-		image: "",
-	},
-	{
-		id: 10,
-		title: "Sonic the Hedgehog 3",
-		details: ["2024", "PG", "Adventure", "1h 49m"],
-		badges: ["HD", "New"],
-		image: "",
-	},
-	{
-		id: 11,
-		title: "PAW Patrol: The Mighty Movie",
-		details: ["2023", "G", "Animation", "1h 28m"],
-		badges: ["HD"],
-		image: "",
-	},
-	{
-		id: 12,
-		title: "The Iron Claw",
-		details: ["2023", "R", "Drama", "2h 12m"],
-		badges: ["HD"],
-		image: "",
-	},
-	{
-		id: 13,
-		title: "Mean Girls",
-		details: ["2024", "PG-13", "Musical", "1h 52m"],
-		badges: ["HD", "New"],
-		image: "",
-	},
-	{
-		id: 14,
-		title: "Pet Sematary: Bloodlines",
-		details: ["2023", "R", "Horror", "1h 27m"],
-		badges: ["HD"],
-		image: "",
-	},
-	{
-		id: 15,
-		title: "Oppenheimer",
-		details: ["2023", "R", "Drama", "3h 0m"],
-		badges: ["4K", "HDR"],
-		image: "",
-	},
-	{
-		id: 16,
-		title: "Gladiator II",
-		details: ["2024", "R", "Action", "2h 28m"],
-		badges: ["4K", "New"],
-		image: "",
-	},
+    {
+        id: 1,
+        title: "Top Gun: Maverick",
+        details: ["2022", "PG-13", "Action", "2h 11m"],
+        badges: ["4K", "HDR"],
+        image: "http://media.themoviedb.org/t/p/w600_and_h900_face/62HCnUTziyWcpDaBO2i1DX17ljH.jpg",
+    },
+    {
+        id: 2,
+        title: "Mission: Impossible — Dead Reckoning",
+        details: ["2023", "PG-13", "Action", "2h 43m"],
+        badges: ["4K", "HDR"],
+        image: "https://media.themoviedb.org/t/p/w600_and_h900_face/NNxYkU70HPurnNCSiCjYAmacwm.jpg",
+    },
+    {
+        id: 3,
+        title: "Transformers: Rise of the Beasts",
+        details: ["2023", "PG-13", "Action", "2h 7m"],
+        badges: ["HD"],
+        image: "https://media.themoviedb.org/t/p/w600_and_h900_face/gPbM0MK8CP8A174rmUwGsADNYKD.jpg",
+    },
+    {
+        id: 4,
+        title: "A Quiet Place: Day One",
+        details: ["2024", "PG-13", "Horror", "1h 39m"],
+        badges: ["HD", "New"],
+        image: "https://media.themoviedb.org/t/p/w600_and_h900_face/hU42CRk14JuPEdqZG3AWmagiPAP.jpg",
+    },
+    {
+        id: 5,
+        title: "Dungeons & Dragons: Honor Among Thieves",
+        details: ["2023", "PG-13", "Fantasy", "2h 14m"],
+        badges: ["HD"],
+        image: "https://media.themoviedb.org/t/p/w600_and_h900_face/v7UF7ypAqjsFZFdjksjQ7IUpXdn.jpg",
+    },
+    {
+        id: 6,
+        title: "Scream VI",
+        details: ["2023", "R", "Horror", "2h 3m"],
+        badges: ["HD"],
+        image: "https://media.themoviedb.org/t/p/w600_and_h900_face/wDWwtvkRRlgTiUr6TyLSMX8FCuZ.jpg",
+    },
+    {
+        id: 7,
+        title: "IF",
+        details: ["2024", "PG", "Comedy", "1h 44m"],
+        badges: ["HD", "New"],
+        image: "https://media.themoviedb.org/t/p/w600_and_h900_face/xbKFv4KF3sVYuWKllLlwWDmuZP7.jpg",
+    },
+    {
+        id: 8,
+        title: "Teenage Mutant Ninja Turtles: Mutant Mayhem",
+        details: ["2023", "PG", "Animation", "1h 39m"],
+        badges: ["HD"],
+        image: "https://media.themoviedb.org/t/p/w600_and_h900_face/gyh0eECE2IqrW8GWl3KoHBfc45j.jpg",
+    },
+    {
+        id: 9,
+        title: "Interstellar",
+        details: ["2014", "PG-13", "Sci-Fi", "2h 49m"],
+        badges: ["4K", "HDR"],
+        image: "https://media.themoviedb.org/t/p/w600_and_h900_face/yQvGrMoipbRoddT0ZR8tPoR7NfX.jpg",
+    },
+    {
+        id: 10,
+        title: "Sonic the Hedgehog 3",
+        details: ["2024", "PG", "Adventure", "1h 49m"],
+        badges: ["HD", "New"],
+        image: "https://media.themoviedb.org/t/p/w600_and_h900_face/d8Ryb8AunYAuycVKDp5HpdWPKgC.jpg",
+    },
+    {
+        id: 11,
+        title: "PAW Patrol: The Mighty Movie",
+        details: ["2023", "G", "Animation", "1h 28m"],
+        badges: ["HD"],
+        image: "https://media.themoviedb.org/t/p/w600_and_h900_face/aTvePCU7exLepwg5hWySjwxojQK.jpg",
+    },
+    {
+        id: 12,
+        title: "The Iron Claw",
+        details: ["2023", "R", "Drama", "2h 12m"],
+        badges: ["HD"],
+        image: "https://media.themoviedb.org/t/p/w600_and_h900_face/s2VLjcykJfwplJNGT5iEeCA0mRx.jpg",
+    },
+    {
+        id: 13,
+        title: "Mean Girls",
+        details: ["2024", "PG-13", "Musical", "1h 52m"],
+        badges: ["HD", "New"],
+        image: "https://media.themoviedb.org/t/p/w600_and_h900_face/2ZkuQXvVhh45uSvkBej4S7Ix1NJ.jpg",
+    },
+    {
+        id: 14,
+        title: "Pet Sematary: Bloodlines",
+        details: ["2023", "R", "Horror", "1h 27m"],
+        badges: ["HD"],
+        image: "https://media.themoviedb.org/t/p/w600_and_h900_face/yqnNLn24shYnZ6kqGpbwuB3NJ0D.jpg",
+    },
+    {
+        id: 15,
+        title: "Oppenheimer",
+        details: ["2023", "R", "Drama", "3h 0m"],
+        badges: ["4K", "HDR"],
+        image: "https://media.themoviedb.org/t/p/w600_and_h900_face/8Gxv8gSFCU0XGDykEGv7zR1n2ua.jpg",
+    },
+    {
+        id: 16,
+        title: "Gladiator II",
+        details: ["2024", "R", "Action", "2h 28m"],
+        badges: ["4K", "New"],
+        image: "https://media.themoviedb.org/t/p/w600_and_h900_face/2cxhvwyEwRlysAmRH4iodkvo0z5.jpg",
+    },
 ];
 
 const Movies = function () {
-	const [currentItemIndex, setCurrentItemIndex] = useState(0);
+    const [currentItemIndex, setCurrentItemIndex] = useState(0);
 
-	const maxItemIndex = MOVIES.length - 1;
+    const maxItemIndex = MOVIES.length - 1;
 
-	const handleItemsIteration = function (e: any) {
-		const { target } = e;
+    const handleItemsIteration = function (e: any) {
+        const { target } = e;
 
-		const button = target.closest("button");
+        const button = target.closest("button");
 
-		const direction = +button.dataset.direction;
+        const direction = +button.dataset.direction;
 
-		setCurrentItemIndex((v) => (direction < 0 ? --v : ++v));
-	};
+        setCurrentItemIndex((v) => (direction < 0 ? --v : ++v));
+    };
 
-	return (
-		<section className="section-movies-container">
-			<header className="header-section-movies-container">
-				<h2>
-					Top <span>Movies</span>
-				</h2>
-				<a href="/movies">
-					<span>Browse All</span>
-					<ChevronRight size={16} />
-				</a>
-			</header>
-			<div className="div-section-movies-container-list-container">
-				{currentItemIndex * -1 < maxItemIndex && (
-					<button onClick={handleItemsIteration} data-direction="-1">
-						<ChevronLeft size={16} />
-					</button>
-				)}
-				<ul className="section-movies-container-list">
-					{MOVIES.map(({ id, title, details, badges, image }) => {
-						return (
-							<li
-								key={id}
-								className="section-movies-container-list-item"
-								style={{ transform: `translateX(calc(${currentItemIndex} * (100% + 16px)))` }}
-							>
-								<div className="div-section-movies-container-list-item-thumbnail-container">
-									<ul className="div-section-movies-container-list-item-thumbnail-container-badges-list">
-										{badges.map((badge, j) => {
-											return (
-												<li key={j} className="div-section-movies-container-list-item-thumbnail-container-badges-list-item">
-													<span className={badge === "4K" ? "four-k" : badge.toLowerCase()}>{badge}</span>
-												</li>
-											);
-										})}
-									</ul>
-									<div className="div-section-movies-container-list-item-thumbnail">
-										{image ? <img src={image} /> : <Clapperboard size={32} />}
-									</div>
-									<button>
-										<Play size={14} />
-									</button>
-								</div>
-								<div className="div-section-movies-container-list-item-info-container">
-									<h4>{title}</h4>
-									<ul className="section-movies-container-list-item-info-container-details-list">
-										{details.map((detail, j) => {
-											return (
-												<li key={j} className="section-movies-container-list-item-info-container-details-list-item">
-													<span>{detail}</span>
-												</li>
-											);
-										})}
-									</ul>
-								</div>
-							</li>
-						);
-					})}
-				</ul>
-				{currentItemIndex !== 0 && (
-					<button onClick={handleItemsIteration} data-direction="1">
-						<ChevronRight size={16} />
-					</button>
-				)}
-			</div>
-		</section>
-	);
+    return (
+        <section className="section-movies-container">
+            <header className="header-section-movies-container">
+                <h2>
+                    Top <span>Movies</span>
+                </h2>
+                <a href="/movies">
+                    <span>Browse All</span>
+                    <ChevronRight size={16} />
+                </a>
+            </header>
+            <div className="div-section-movies-container-list-container">
+                {currentItemIndex * -1 < maxItemIndex && (
+                    <button onClick={handleItemsIteration} data-direction="-1">
+                        <ChevronLeft size={16} />
+                    </button>
+                )}
+                <ul className="section-movies-container-list">
+                    {MOVIES.map(({ id, title, details, badges, image }) => {
+                        return (
+                            <li key={id} className="section-movies-container-list-item" style={{ transform: `translateX(calc(${currentItemIndex} * (100% + 16px)))` }}>
+                                <div className="div-section-movies-container-list-item-thumbnail-container">
+                                    <ul className="div-section-movies-container-list-item-thumbnail-container-badges-list">
+                                        {badges.map((badge, j) => {
+                                            return (
+                                                <li key={j} className="div-section-movies-container-list-item-thumbnail-container-badges-list-item">
+                                                    <span className={badge === "4K" ? "four-k" : badge.toLowerCase()}>{badge}</span>
+                                                </li>
+                                            );
+                                        })}
+                                    </ul>
+                                    <div className="div-section-movies-container-list-item-thumbnail">{image ? <img src={image} /> : <Clapperboard size={32} />}</div>
+                                    <button>
+                                        <Play size={14} />
+                                    </button>
+                                </div>
+                                <div className="div-section-movies-container-list-item-info-container">
+                                    <h4>{title}</h4>
+                                    <ul className="section-movies-container-list-item-info-container-details-list">
+                                        {details.map((detail, j) => {
+                                            return (
+                                                <li key={j} className="section-movies-container-list-item-info-container-details-list-item">
+                                                    <span>{detail}</span>
+                                                </li>
+                                            );
+                                        })}
+                                    </ul>
+                                </div>
+                            </li>
+                        );
+                    })}
+                </ul>
+                {currentItemIndex !== 0 && (
+                    <button onClick={handleItemsIteration} data-direction="1">
+                        <ChevronRight size={16} />
+                    </button>
+                )}
+            </div>
+        </section>
+    );
 };
 
 export default Movies;
--- pinnacle-streams/source/views/Home/partials/Shows/Shows.tsx
+++ pinnacle-streams/source/views/Home/partials/Shows/Shows.tsx
@@ -7,201 +7,195 @@ import type Show from "./types/Show";
 import "./css/Shows.css";
 
 const SHOWS: Show[] = [
-	{
-		id: 1,
-		title: "Yellowstone",
-		details: ["2018", "TV-MA", "Drama", "5 Seasons"],
-		badges: ["HD"],
-		image: "https://media.themoviedb.org/t/p/w600_and_h900_face/vOYfRZ0NpUK5hG2CB2dJFnYJlGe.jpg",
-	},
-	{
-		id: 2,
-		title: "Tulsa King",
-		details: ["2022", "TV-MA", "Crime", "2 Seasons"],
-		badges: ["HD", "New"],
-		image: "",
-	},
-	{
-		id: 3,
-		title: "Star Trek: Strange New Worlds",
-		details: ["2022", "TV-14", "Sci-Fi", "3 Seasons"],
-		badges: ["4K", "New"],
-		image: "",
-	},
-	{
-		id: 4,
-		title: "The Good Fight",
-		details: ["2017", "TV-MA", "Legal Drama", "6 Seasons"],
-		badges: ["HD"],
-		image: "",
-	},
-	{
-		id: 5,
-		title: "Halo",
-		details: ["2022", "TV-MA", "Sci-Fi", "2 Seasons"],
-		badges: ["4K"],
-		image: "",
-	},
-	{
-		id: 6,
-		title: "Lioness",
-		details: ["2023", "TV-MA", "Action", "2 Seasons"],
-		badges: ["HD", "New"],
-		image: "",
-	},
-	{
-		id: 7,
-		title: "Mayor of Kingstown",
-		details: ["2021", "TV-MA", "Crime", "3 Seasons"],
-		badges: ["HD"],
-		image: "",
-	},
-	{
-		id: 8,
-		title: "Star Trek: Discovery",
-		details: ["2017", "TV-14", "Sci-Fi", "5 Seasons"],
-		badges: ["4K"],
-		image: "",
-	},
-	{
-		id: 9,
-		title: "1923",
-		details: ["2022", "TV-MA", "Drama", "2 Seasons"],
-		badges: ["HD", "Original"],
-		image: "",
-	},
-	{
-		id: 10,
-		title: "1883",
-		details: ["2021", "TV-MA", "Western", "1 Season"],
-		badges: ["HD", "Original"],
-		image: "",
-	},
-	{
-		id: 11,
-		title: "The Offer",
-		details: ["2022", "TV-MA", "Drama", "1 Season"],
-		badges: ["HD"],
-		image: "",
-	},
-	{
-		id: 12,
-		title: "Star Trek: Picard",
-		details: ["2020", "TV-14", "Sci-Fi", "3 Seasons"],
-		badges: ["4K"],
-		image: "",
-	},
-	{
-		id: 13,
-		title: "Evil",
-		details: ["2019", "TV-MA", "Supernatural", "4 Seasons"],
-		badges: ["HD"],
-		image: "",
-	},
-	{
-		id: 14,
-		title: "Seal Team",
-		details: ["2017", "TV-14", "Action", "7 Seasons"],
-		badges: ["HD"],
-		image: "",
-	},
-	{
-		id: 15,
-		title: "iCarly",
-		details: ["2021", "TV-14", "Comedy", "3 Seasons"],
-		badges: ["HD"],
-		image: "",
-	},
-	{
-		id: 16,
-		title: "Grease: Rise of the Pink Ladies",
-		details: ["2023", "TV-14", "Musical", "1 Season"],
-		badges: ["HD"],
-		image: "",
-	},
+    {
+        id: 1,
+        title: "Yellowstone",
+        details: ["2018", "TV-MA", "Drama", "5 Seasons"],
+        badges: ["HD"],
+        image: "https://media.themoviedb.org/t/p/w600_and_h900_face/vOYfRZ0NpUK5hG2CB2dJFnYJlGe.jpg",
+    },
+    {
+        id: 2,
+        title: "Tulsa King",
+        details: ["2022", "TV-MA", "Crime", "2 Seasons"],
+        badges: ["HD", "New"],
+        image: "https://media.themoviedb.org/t/p/w600_and_h900_face/rOYLWCdAifpUtPlTf1WHxyaxeMt.jpg",
+    },
+    {
+        id: 3,
+        title: "Star Trek: Strange New Worlds",
+        details: ["2022", "TV-14", "Sci-Fi", "3 Seasons"],
+        badges: ["4K", "New"],
+        image: "https://media.themoviedb.org/t/p/w600_and_h900_face/w6KapI2JvrCkOPmQhkwYPJNjqeo.jpg",
+    },
+    {
+        id: 4,
+        title: "The Good Fight",
+        details: ["2017", "TV-MA", "Legal Drama", "6 Seasons"],
+        badges: ["HD"],
+        image: "https://media.themoviedb.org/t/p/w600_and_h900_face/8qoOHOfbUbrCcHZnDVxGcwOWinV.jpg",
+    },
+    {
+        id: 5,
+        title: "Halo",
+        details: ["2022", "TV-MA", "Sci-Fi", "2 Seasons"],
+        badges: ["4K"],
+        image: "https://media.themoviedb.org/t/p/w600_and_h900_face/4UmNhZCEu8Vt3byMvNxNEPyf8EY.jpg",
+    },
+    {
+        id: 6,
+        title: "Lioness",
+        details: ["2023", "TV-MA", "Action", "2 Seasons"],
+        badges: ["HD", "New"],
+        image: "https://media.themoviedb.org/t/p/w600_and_h900_face/ajaXSmdAlYYhnvx1EIsvpfN949y.jpg",
+    },
+    {
+        id: 7,
+        title: "Mayor of Kingstown",
+        details: ["2021", "TV-MA", "Crime", "3 Seasons"],
+        badges: ["HD"],
+        image: "https://media.themoviedb.org/t/p/w600_and_h900_face/6rWIip9MZELAA0SKii5WqsBDCYW.jpg",
+    },
+    {
+        id: 8,
+        title: "Star Trek: Discovery",
+        details: ["2017", "TV-14", "Sci-Fi", "5 Seasons"],
+        badges: ["4K"],
+        image: "https://media.themoviedb.org/t/p/w600_and_h900_face/xwpOHgym48Ftz7fbJq5te5xoiwu.jpg",
+    },
+    {
+        id: 9,
+        title: "1923",
+        details: ["2022", "TV-MA", "Drama", "2 Seasons"],
+        badges: ["HD", "Original"],
+        image: "https://media.themoviedb.org/t/p/w600_and_h900_face/zgZRJZvZn5cpsWAB0zMUdad3iZd.jpg",
+    },
+    {
+        id: 10,
+        title: "1883",
+        details: ["2021", "TV-MA", "Western", "1 Season"],
+        badges: ["HD", "Original"],
+        image: "https://media.themoviedb.org/t/p/w600_and_h900_face/waLbm384SQDwLTCn6ttPqQS5kfV.jpg",
+    },
+    {
+        id: 11,
+        title: "The Offer",
+        details: ["2022", "TV-MA", "Drama", "1 Season"],
+        badges: ["HD"],
+        image: "https://media.themoviedb.org/t/p/w600_and_h900_face/9fdYX5uc3HZEuYotms3XJ72AbCr.jpg",
+    },
+    {
+        id: 12,
+        title: "Star Trek: Picard",
+        details: ["2020", "TV-14", "Sci-Fi", "3 Seasons"],
+        badges: ["4K"],
+        image: "https://media.themoviedb.org/t/p/w600_and_h900_face/nIlAKIrLKxOeoEnc0Urb65yNCp.jpg",
+    },
+    {
+        id: 13,
+        title: "Evil",
+        details: ["2019", "TV-MA", "Supernatural", "4 Seasons"],
+        badges: ["HD"],
+        image: "https://media.themoviedb.org/t/p/w600_and_h900_face/auRAAVxlyT6YACql96UdtnpFHy3.jpg",
+    },
+    {
+        id: 14,
+        title: "Seal Team",
+        details: ["2017", "TV-14", "Action", "7 Seasons"],
+        badges: ["HD"],
+        image: "https://media.themoviedb.org/t/p/w600_and_h900_face/6iSnec83UEMgLOXAtGYwaVNHLjR.jpg",
+    },
+    {
+        id: 15,
+        title: "iCarly",
+        details: ["2021", "TV-14", "Comedy", "3 Seasons"],
+        badges: ["HD"],
+        image: "https://media.themoviedb.org/t/p/w600_and_h900_face/tM4P2RNIIFzFJi7UngNVB80sRCp.jpg",
+    },
+    {
+        id: 16,
+        title: "Grease: Rise of the Pink Ladies",
+        details: ["2023", "TV-14", "Musical", "1 Season"],
+        badges: ["HD"],
+        image: "https://media.themoviedb.org/t/p/w600_and_h900_face/dGu8u5F9Cjn9Ij2aMYtGmKHp5m1.jpg",
+    },
 ];
 
 const Shows = function () {
-	const [currentItemIndex, setCurrentItemIndex] = useState(0);
+    const [currentItemIndex, setCurrentItemIndex] = useState(0);
 
-	const maxItemIndex = SHOWS.length - 1;
+    const maxItemIndex = SHOWS.length - 1;
 
-	const handleItemsIteration = function (e: any) {
-		const { target } = e;
+    const handleItemsIteration = function (e: any) {
+        const { target } = e;
 
-		const button = target.closest("button");
+        const button = target.closest("button");
 
-		const direction = +button.dataset.direction;
+        const direction = +button.dataset.direction;
 
-		setCurrentItemIndex((v) => (direction < 0 ? --v : ++v));
-	};
+        setCurrentItemIndex((v) => (direction < 0 ? --v : ++v));
+    };
 
-	return (
-		<section className="section-shows-container">
-			<header className="header-section-shows-container">
-				<h2>
-					Top <span>Shows</span>
-				</h2>
-				<a href="/movies">
-					<span>Browse All</span>
-					<ChevronRight size={16} />
-				</a>
-			</header>
-			<div className="div-section-shows-container-list-container">
-				{currentItemIndex * -1 < maxItemIndex && (
-					<button onClick={handleItemsIteration} data-direction="-1">
-						<ChevronLeft size={16} />
-					</button>
-				)}
-				<ul className="section-shows-container-list">
-					{SHOWS.map(({ id, title, details, badges, image }) => {
-						return (
-							<li
-								key={id}
-								className="section-shows-container-list-item"
-								style={{ transform: `translateX(calc(${currentItemIndex} * (100% + 16px)))` }}
-							>
-								<div className="div-section-shows-container-list-item-thumbnail-container">
-									<ul className="div-section-shows-container-list-item-thumbnail-container-badges-list">
-										{badges.map((badge, j) => {
-											return (
-												<li key={j} className="div-section-shows-container-list-item-thumbnail-container-badges-list-item">
-													<span className={badge === "4K" ? "four-k" : badge.toLowerCase()}>{badge}</span>
-												</li>
-											);
-										})}
-									</ul>
-									<div className="div-section-shows-container-list-item-thumbnail">
-										{image ? <img src={image} /> : <Clapperboard size={32} />}
-									</div>
-									<button>
-										<Play size={14} />
-									</button>
-								</div>
-								<div className="div-section-shows-container-list-item-info-container">
-									<h4>{title}</h4>
-									<ul className="section-shows-container-list-item-info-container-details-list">
-										{details.map((detail, j) => {
-											return (
-												<li key={j} className="section-shows-container-list-item-info-container-details-list-item">
-													<span>{detail}</span>
-												</li>
-											);
-										})}
-									</ul>
-								</div>
-							</li>
-						);
-					})}
-				</ul>
-				{currentItemIndex !== 0 && (
-					<button onClick={handleItemsIteration} data-direction="1">
-						<ChevronRight size={16} />
-					</button>
-				)}
-			</div>
-		</section>
-	);
+    return (
+        <section className="section-shows-container">
+            <header className="header-section-shows-container">
+                <h2>
+                    Top <span>Shows</span>
+                </h2>
+                <a href="/movies">
+                    <span>Browse All</span>
+                    <ChevronRight size={16} />
+                </a>
+            </header>
+            <div className="div-section-shows-container-list-container">
+                {currentItemIndex * -1 < maxItemIndex && (
+                    <button onClick={handleItemsIteration} data-direction="-1">
+                        <ChevronLeft size={16} />
+                    </button>
+                )}
+                <ul className="section-shows-container-list">
+                    {SHOWS.map(({ id, title, details, badges, image }) => {
+                        return (
+                            <li key={id} className="section-shows-container-list-item" style={{ transform: `translateX(calc(${currentItemIndex} * (100% + 16px)))` }}>
+                                <div className="div-section-shows-container-list-item-thumbnail-container">
+                                    <ul className="div-section-shows-container-list-item-thumbnail-container-badges-list">
+                                        {badges.map((badge, j) => {
+                                            return (
+                                                <li key={j} className="div-section-shows-container-list-item-thumbnail-container-badges-list-item">
+                                                    <span className={badge === "4K" ? "four-k" : badge.toLowerCase()}>{badge}</span>
+                                                </li>
+                                            );
+                                        })}
+                                    </ul>
+                                    <div className="div-section-shows-container-list-item-thumbnail">{image ? <img src={image} /> : <Clapperboard size={32} />}</div>
+                                    <button>
+                                        <Play size={14} />
+                                    </button>
+                                </div>
+                                <div className="div-section-shows-container-list-item-info-container">
+                                    <h4>{title}</h4>
+                                    <ul className="section-shows-container-list-item-info-container-details-list">
+                                        {details.map((detail, j) => {
+                                            return (
+                                                <li key={j} className="section-shows-container-list-item-info-container-details-list-item">
+                                                    <span>{detail}</span>
+                                                </li>
+                                            );
+                                        })}
+                                    </ul>
+                                </div>
+                            </li>
+                        );
+                    })}
+                </ul>
+                {currentItemIndex !== 0 && (
+                    <button onClick={handleItemsIteration} data-direction="1">
+                        <ChevronRight size={16} />
+                    </button>
+                )}
+            </div>
+        </section>
+    );
 };
 
 export default Shows;
