b4048f4a3f31297e073711a5040c9d87dd6ef488
Balša Bazović <balsa.bazovic@gmail.com>
2026-03-23 15:29:27 -0400

    Updated Auth partial views.

--- pinnacle-streams/source/views/Auth/partials/Signin/Signin.tsx
+++ pinnacle-streams/source/views/Auth/partials/Signin/Signin.tsx
@@ -1,55 +1,66 @@
 // IMPORT CORE MODULES
+import { useState } from "react";
 import { Link } from "react-router-dom";
-import { Check, Lock, Mail } from "lucide-react";
+import { Check, Eye, EyeOff, Lock, Mail } from "lucide-react";
 // IMPORTED STYLESHEETS
 import "./css/Signin.css";
 
 const Signin = function () {
-	return (
-		<div className="div-signin-view-container">
-			<div className="div-signin-view-top-container">
-				<header className="header-signin-view-top-container">
-					<h2>Welcome Back</h2>
-					<p>Sign in to continue watching.</p>
-				</header>
-				<div className="div-signin-view-top-actions-container">
-					<button>Google</button>
-					<button>Apple</button>
-				</div>
-			</div>
-			<div className="div-signin-view-bottom-container">
-				<form className="form-signin-view-container" action="/auth/signin" method="POST">
-					<div className="div-form-signin-view-input-container">
-						<label htmlFor="email">Email Address</label>
-						<div className="div-form-signin-view-input">
-							<input id="email" type="email" name="email" placeholder="you@example.com" />
-							<Mail size={16} />
-						</div>
-					</div>
-					<div className="div-form-signin-view-input-container">
-						<label htmlFor="password">Password</label>
-						<div className="div-form-signin-view-input">
-							<input id="password" type="password" name="password" placeholder="Enter your password..." />
-							<Lock size={16} />
-						</div>
-					</div>
-					<div className="div-form-signin-view-actions-container">
-						<div className="div-form-signin-view-actions-checkbox-container">
-							<button type="button">
-								<Check size={12} />
-							</button>
-							<span>Remember me?</span>
-						</div>
-						<Link to="/password-reset">Forgot password?</Link>
-					</div>
-					<button type="submit">Sign In</button>
-				</form>
-				<p>
-					Don't have an account? <Link to="/auth/signup">Sign up for free.</Link>
-				</p>
-			</div>
-		</div>
-	);
+    const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+    const [isRememberMeChecked, setIsRememberMeChecked] = useState(false);
+
+    const handleTogglePasswordVisible = () => setIsPasswordVisible((v) => !v);
+
+    const handleToggleRememberMe = () => setIsRememberMeChecked((v) => !v);
+
+    return (
+        <div className="div-signin-view-container">
+            <div className="div-signin-view-top-container">
+                <header className="header-signin-view-top-container">
+                    <h2>Welcome Back</h2>
+                    <p>Sign in to continue watching.</p>
+                </header>
+                <div className="div-signin-view-top-actions-container">
+                    <button>Google</button>
+                    <button>Apple</button>
+                </div>
+            </div>
+            <div className="div-signin-view-bottom-container">
+                <form className="form-signin-view-container" action="/auth/signin" method="POST">
+                    <div className="div-form-signin-view-input-container">
+                        <label htmlFor="email">Email Address</label>
+                        <div className="div-form-signin-view-input">
+                            <Mail size={16} />
+                            <input id="email" type="email" name="email" autoComplete="on" placeholder="you@example.com" />
+                        </div>
+                    </div>
+                    <div className="div-form-signin-view-input-container">
+                        <label htmlFor="password">Password</label>
+                        <div className="div-form-signin-view-input">
+                            <Lock size={16} />
+                            <input id="password" type={isPasswordVisible ? "text" : "password"} name="password" autoComplete="on" placeholder="Enter your password..." />
+                            <button onClick={handleTogglePasswordVisible} type="button">
+                                {isPasswordVisible ? <EyeOff size={16} /> : <Eye size={16} />}
+                            </button>
+                        </div>
+                    </div>
+                    <div className="div-form-signin-view-actions-container">
+                        <div className="div-form-signin-view-actions-checkbox-container">
+                            <button className={isRememberMeChecked ? "checked" : ""} onClick={handleToggleRememberMe} type="button">
+                                <Check size={14} />
+                            </button>
+                            <span>Remember me?</span>
+                        </div>
+                        <Link to="/password-reset">Forgot password?</Link>
+                    </div>
+                    <button type="submit">Sign In</button>
+                </form>
+                <p>
+                    Don't have an account? <Link to="/auth/signup">Sign up for free.</Link>
+                </p>
+            </div>
+        </div>
+    );
 };
 
 export default Signin;
--- pinnacle-streams/source/views/Auth/partials/Signup/Signup.tsx
+++ pinnacle-streams/source/views/Auth/partials/Signup/Signup.tsx
@@ -1,70 +1,81 @@
 // IMPORT CORE MODULES
+import { useState } from "react";
 import { Link } from "react-router-dom";
-import { Check, Lock, Mail, User } from "lucide-react";
+import { Check, Eye, EyeOff, Lock, Mail, User } from "lucide-react";
 // IMPORTED STYLESHEETS
 import "./css/Signup.css";
 
 const Signup = function () {
-	return (
-		<div className="div-signup-view-container">
-			<div className="div-signup-view-top-container">
-				<header className="header-signup-view-top-container">
-					<h2>Create Your Account</h2>
-					<p>Start your 7-day free trial today.</p>
-				</header>
-				<div className="div-signup-view-top-actions-container">
-					<button>Google</button>
-					<button>Apple</button>
-				</div>
-			</div>
-			<div className="div-signup-view-bottom-container">
-				<form className="form-signup-view-container" action="/auth/signin" method="POST">
-					<div className="div-form-signup-view-multi-inputs-container">
-						<div className="div-form-signup-view-input-container">
-							<label htmlFor="first-name">First Name</label>
-							<div className="div-form-signup-view-input">
-								<input id="first-name" type="text" name="first-name" placeholder="John" />
-								<User size={16} />
-							</div>
-						</div>
-						<div className="div-form-signup-view-input-container">
-							<label htmlFor="last-name">Last Name</label>
-							<div className="div-form-signup-view-input">
-								<input id="last-name" type="text" name="last-name" placeholder="Doe" />
-								<User size={16} />
-							</div>
-						</div>
-					</div>
-					<div className="div-form-signup-view-input-container">
-						<label htmlFor="email">Email Address</label>
-						<div className="div-form-signup-view-input">
-							<input id="email" type="email" name="email" placeholder="you@example.com" />
-							<Mail size={16} />
-						</div>
-					</div>
-					<div className="div-form-signup-view-input-container">
-						<label htmlFor="password">Password</label>
-						<div className="div-form-signup-view-input">
-							<input id="password" type="password" name="password" placeholder="Enter your password..." />
-							<Lock size={16} />
-						</div>
-					</div>
-					<div className="div-form-signup-view-actions-checkbox-container">
-						<button type="button">
-							<Check size={12} />
-						</button>
-						<p>
-							I agree to the <Link to="/terms-of-service">Terms of Service</Link> and <Link to="/privacy-policy">Privacy Policy</Link>.
-						</p>
-					</div>
-					<button type="submit">Create Account</button>
-				</form>
-				<p>
-					Already have an account? <Link to="/auth/signup">Sign in.</Link>
-				</p>
-			</div>
-		</div>
-	);
+    const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+    const [isAgreementChecked, setIsAgreementChecked] = useState(false);
+
+    const handleTogglePasswordVisible = () => setIsPasswordVisible((v) => !v);
+
+    const handleToggleRememberMe = () => setIsAgreementChecked((v) => !v);
+
+    return (
+        <div className="div-signup-view-container">
+            <div className="div-signup-view-top-container">
+                <header className="header-signup-view-top-container">
+                    <h2>Create Your Account</h2>
+                    <p>Start your 7-day free trial today.</p>
+                </header>
+                <div className="div-signup-view-top-actions-container">
+                    <button>Google</button>
+                    <button>Apple</button>
+                </div>
+            </div>
+            <div className="div-signup-view-bottom-container">
+                <form className="form-signup-view-container" action="/auth/signin" method="POST">
+                    <div className="div-form-signup-view-multi-inputs-container">
+                        <div className="div-form-signup-view-input-container">
+                            <label htmlFor="first-name">First Name</label>
+                            <div className="div-form-signup-view-input">
+                                <User size={16} />
+                                <input id="first-name" type="text" name="first-name" autoComplete="on" placeholder="John" />
+                            </div>
+                        </div>
+                        <div className="div-form-signup-view-input-container">
+                            <label htmlFor="last-name">Last Name</label>
+                            <div className="div-form-signup-view-input">
+                                <User size={16} />
+                                <input id="last-name" type="text" name="last-name" autoComplete="on" placeholder="Doe" />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="div-form-signup-view-input-container">
+                        <label htmlFor="email">Email Address</label>
+                        <div className="div-form-signup-view-input">
+                            <Mail size={16} />
+                            <input id="email" type="email" name="email" autoComplete="on" placeholder="you@example.com" />
+                        </div>
+                    </div>
+                    <div className="div-form-signup-view-input-container">
+                        <label htmlFor="password">Password</label>
+                        <div className="div-form-signup-view-input">
+                            <Lock size={16} />
+                            <input id="password" type={isPasswordVisible ? "text" : "password"} name="password" autoComplete="on" placeholder="Enter your password..." />
+                            <button onClick={handleTogglePasswordVisible} type="button">
+                                {isPasswordVisible ? <EyeOff size={16} /> : <Eye size={16} />}
+                            </button>
+                        </div>
+                    </div>
+                    <div className="div-form-signup-view-actions-checkbox-container">
+                        <button className={isAgreementChecked ? "checked" : ""} onClick={handleToggleRememberMe} type="button">
+                            <Check size={14} />
+                        </button>
+                        <p>
+                            I agree to the <Link to="/terms-of-service">Terms of Service</Link> and <Link to="/privacy-policy">Privacy Policy</Link>.
+                        </p>
+                    </div>
+                    <button type="submit">Create Account</button>
+                </form>
+                <p>
+                    Already have an account? <Link to="/auth/signup">Sign in.</Link>
+                </p>
+            </div>
+        </div>
+    );
 };
 
 export default Signup;
