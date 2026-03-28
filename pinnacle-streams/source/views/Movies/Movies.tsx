21f452fbd21aa246ae25ec41ed5aec1173a607b4
Balša Bazović <balsa.bazovic@gmail.com>
2026-03-28 10:02:54 -0400

    Updated Movies view.

--- pinnacle-streams/source/views/Movies/Movies.tsx
+++ pinnacle-streams/source/views/Movies/Movies.tsx
@@ -1,11 +1,15 @@
 // IMPORTED CORE MODULES
-import { type ReactElement } from "react";
+import { useEffect, type ReactElement } from "react";
 // IMPORTED STYLESHEETS
 import "./css/Movies.css";
 // IMPORTED CUSTOM MODULES
 import Nav from "../../partials/Nav/Nav";
 
 const Movies = function (): ReactElement {
+    useEffect(() => {
+        document.title = "Pinnacle Streams | Movies";
+    }, []);
+
     return (
         <>
             <Nav isMainNav={false} />
