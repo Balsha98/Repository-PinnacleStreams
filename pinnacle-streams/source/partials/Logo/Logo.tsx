37842969ecdb822277d182e1f6d9f9c25d109563
Balša Bazović <balsa.bazovic@gmail.com>
2026-04-21 06:51:07 -0400

    Updated Logo partial component.

--- pinnacle-streams/source/partials/Logo/Logo.tsx
+++ pinnacle-streams/source/partials/Logo/Logo.tsx
@@ -3,12 +3,15 @@ import type { ReactElement } from "react";
 import { Mountain } from "lucide-react";
 // IMPORTED STYLESHEETS
 import "./css/Logo.css";
+import { Link } from "react-router-dom";
 
 const Logo = function (): ReactElement {
     return (
         <div className="div-main-logo-container">
-            <Mountain size={24} />
-            <h2>Paramount</h2>
+            <Link to="/home">
+                <Mountain size={24} />
+                <h2>Paramount</h2>
+            </Link>
         </div>
     );
 };
